fef22655979a78f3e41108fbda91db8423856461
Antu <prince.anto97@gmai.com>
2020-06-16 17:24:27 +0600

    Updates

---
+++ precache-manifest.9a174daa7eac3ba13710fdae75fd6cc4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a9865c9a6c8d2bb2608736147bba3f",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "7ab7d7a5bf7e314f59d4",
+    "url": "/react-deploy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dc9da6bb7301ff3b6126",
+    "url": "/react-deploy/static/css/main.acca757c.chunk.css"
+  },
+  {
+    "revision": "7ab7d7a5bf7e314f59d4",
+    "url": "/react-deploy/static/js/2.acc929fb.chunk.js"
+  },
+  {
+    "revision": "90736b45c8c5b4f66c75e275e19ff3c9",
+    "url": "/react-deploy/static/js/2.acc929fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9da6bb7301ff3b6126",
+    "url": "/react-deploy/static/js/main.b14989b6.chunk.js"
+  },
+  {
+    "revision": "4264f4839e47d10d9421",
+    "url": "/react-deploy/static/js/runtime-main.a01cc3b2.js"
+  },
+  {
+    "revision": "383932b96c78f8339b9d514b18bb9384",
+    "url": "/react-deploy/static/media/antu.383932b9.jpg"
+  },
+  {
+    "revision": "795b025d5146296a39da340510641da7",
+    "url": "/react-deploy/static/media/antu.795b025d.jpg"
+  },
+  {
+    "revision": "d35f956cd5c110dc86e76a4864436b5b",
+    "url": "/react-deploy/static/media/app1.d35f956c.PNG"
+  },
+  {
+    "revision": "98f3d298e6f5554848b1a4672af3896f",
+    "url": "/react-deploy/static/media/app2.98f3d298.PNG"
+  },
+  {
+    "revision": "ccab71cc63f7cad7d6ef9ee0d932a572",
+    "url": "/react-deploy/static/media/app3.ccab71cc.PNG"
+  },
+  {
+    "revision": "0e2a0f45aa11c1f735b91016e204beb4",
+    "url": "/react-deploy/static/media/design1.0e2a0f45.PNG"
+  },
+  {
+    "revision": "cb182e0eecc08eacbe85c574f74b5d8f",
+    "url": "/react-deploy/static/media/design2.cb182e0e.PNG"
+  },
+  {
+    "revision": "b6058f6d2b6d42d6474d7c08639fd92a",
+    "url": "/react-deploy/static/media/design3.b6058f6d.PNG"
+  },
+  {
+    "revision": "81d9095c6d7130f81bc52fb6e32f0c36",
+    "url": "/react-deploy/static/media/edu.81d9095c.jpg"
+  },
+  {
+    "revision": "37a3ed2b2ed4b699a631467c2f6d25c9",
+    "url": "/react-deploy/static/media/soft.37a3ed2b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sowrov/precache-manifest.cdb07cd6c587837ec9decc3e98aed495.js"
+  "/react-deploy/precache-manifest.9a174daa7eac3ba13710fdae75fd6cc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sowrov/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
